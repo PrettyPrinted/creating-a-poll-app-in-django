c933f0c3f639ff98cdaddfe057b6b2f54a2c3121
divyanshu <divyanshukrishna11@gmail.com>
2024-01-22 11:46:21 +0530

    adding_httpResponse

--- poll_project/poll/views.py
+++ poll_project/poll/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import redirect,HttpResponse
 
 from .forms import CreatePollForm
 from .models import Poll
